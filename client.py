b0b22d73ddfa6d24b8a1a896d2739cc2fdd1b457
Rus <dddrsln@mail.ru>
2023-08-01 10:30:54 +0600

    Fixed according to comments

--- client.py
+++ client.py
@@ -7,22 +7,33 @@ PORT = 12345
 
 
 def send_msg(s: socket.socket, username: str) -> None:
-    while True:
-        message = input()
-        if message == "/exit":
-            s.sendall(f"User {username} has left".encode())
-            s.close()
-            break
-        chat_msg = f"{username}: {message}"
-        s.sendall(chat_msg.encode())
+    try:
+        while True:
+            message = input()
+            if message == "/exit":
+                s.sendall(f"User {username} has left the chat".encode())
+                s.close()
+                break
+            chat_msg = f"{username}: {message}"
+            s.sendall(chat_msg.encode())
+    except ConnectionError as e:
+        print(e)
 
 
 def get_msg(s: socket.socket) -> None:
-    while True:
-        data = s.recv(1024)
-        if not data:
-            break
-        print(f"{data.decode()}")
+    try:
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            print(f"{data.decode()}")
+    except ConnectionError as e:
+        print(e)
+
+
+def chat(s:socket.socket, username: str) -> None:
+    get_msg(s)
+    send_msg(s, username)
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -31,11 +42,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     username = input("Your name: ")
     print(f'Welcome to chat, {username}!\n"/exit" - leave the chat')
 
-    thr1 = Thread(target=send_msg, args=(s, username))
-    thr2 = Thread(target=get_msg, args=(s,))
-
+    thr1 = Thread(target=chat, args=(s, username))
     thr1.start()
-    thr2.start()
-
     thr1.join()
-    thr2.join()
--- server.py
+++ server.py
@@ -2,6 +2,16 @@ import socket
 
 from datetime import datetime
 from threading import Thread
+import logging
+
+console_out = logging.StreamHandler()
+
+logging.basicConfig(
+    handlers=(console_out,),
+    format="[%(asctime)s | %(levelname)s]: %(message)s",
+    datefmt="%m.%d.%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -15,7 +25,7 @@ def new_client(connection: socket.socket, addr: tuple) -> None:
             data = connection.recv(1024)
             if not data:
                 break
-            print(f"{current_time} - {addr} - {data.decode()}")
+            logging.info(f'{data.decode()}')
             for user in users:
                 if user is not connection:
                     user.sendall(data)
@@ -26,12 +36,12 @@ def new_client(connection: socket.socket, addr: tuple) -> None:
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    print(f"Chat server has been started")
+    logging.info("Chat server has been started")
 
     while True:
         conn, addr = s.accept()
         current_time = datetime.now().time()
-        print(f"{current_time} - User {addr} has joined")
+        logging.info(f"{current_time} - User {addr} has joined")
         users.append(conn)
 
         thr = Thread(target=new_client, args=(conn, addr))
